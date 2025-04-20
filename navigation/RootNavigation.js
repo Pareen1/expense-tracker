7b1d3b642b00dd79f202c29041edd569ce6c89dd
Pareen <pareenv@gmail.com>
2025-04-20 21:32:54 +0200

    feat: authentication based navigation

---
+++ navigation/RootNavigation.js
@@ -0,0 +1,100 @@
+import { useContext } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { GlobalStyles } from "../constants/styles";
+import ManageExpenses from "../screens/ManageExpenses";
+import { AuthContext } from "../store/auth-context";
+import SigninScreen from "../screens/SigninScreen";
+import SignupScreen from "../screens/SignupScreen";
+import RecentExpenses from "../screens/RecentExpenses";
+import AllExpenses from "../screens/AllExpenses";
+import IconButton from "../components/UI/IconButton";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary300 },
+        headerTintColor: GlobalStyles.colors.white,
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary600 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate("ManageExpenses");
+            }}
+          />
+        ),
+      })}
+    >
+      <Stack.Screen
+        name="Recent Expenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="hourglass" size={24} color={color} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="All Expenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="calendar" size={24} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+}
+
+function RootNavigation() {
+  const authCtx = useContext(AuthContext);
+  const isAuthenticated = authCtx.isAuthenticated;
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary300,
+        },
+        headerTintColor: "white",
+      }}
+    >
+      {isAuthenticated ? (
+        <>
+          <Stack.Screen
+            name="Expenses Overview"
+            component={ExpensesOverview}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ManageExpenses"
+            component={ManageExpenses}
+            options={{ presentation: "modal" }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Signin" component={SigninScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+}
+
+export default RootNavigation;
